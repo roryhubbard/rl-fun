4e11d262ba225b37adfd5d92cf1c11f700d23961
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-03-25 22:24:06 -0500

    finished sarsa script

--- temporal_difference/sarsa.py
+++ temporal_difference/sarsa.py
@@ -1,36 +1,28 @@
-import random
 import gym
-import numpy as np
+import matplotlib.pyplot as plt
+from utils import initialize_state_action_values, get_greedy_action, \
+    get_epsilon_greedy_action, watch_greedy_policy
+
+
+plt.rcParams['figure.figsize'] = [16, 10]
+plt.rcParams['savefig.facecolor'] = 'black'
+plt.rcParams['figure.facecolor'] = 'black'
+plt.rcParams['figure.edgecolor'] = 'white'
+plt.rcParams['axes.facecolor'] = 'black'
+plt.rcParams['axes.edgecolor'] = 'white'
+plt.rcParams['axes.labelcolor'] = 'white'
+plt.rcParams['axes.titlecolor'] = 'white'
+plt.rcParams['xtick.color'] = 'white'
+plt.rcParams['ytick.color'] = 'white'
+# plt.rcParams['legend.facecolor'] = 'white'
+plt.rcParams['text.color'] = 'white'
+plt.rcParams["figure.autolayout"] = True
 
 
 SEED = 0
 
 
-def initialize_state_action_values(n_states, n_actions, random=False):
-    """
-    Input:
-    - n_states: number of states in environment
-    - n_actions: number of actions in environment
-    - random: boolean that decides to initialize state-action values randomly
-      or make them all equal
-
-    Output:
-    - Q: state-action value function
-    """
-    if random:
-        Q = np.random.default_rng(SEED).dirichlet(np.ones(n_actions),
-                                                  size=n_states)
-    else:
-        # all state-action values are equal
-        Q = np.ones((n_states, n_actions)) / n_actions
-    return Q
-
-
-def get_greedy_action(Q, observation):
-    return Q[observation].argmax()
-
-
-def sarsa(env, num_episodes, discount_rate=1.0, learning_rate=0.5, epsilon=0.2):
+def sarsa(env, num_episodes, discount_rate=1.0, alpha=0.5, epsilon=0.1):
     """
     Estimate optimal state-value function Q using SARSA.
 
@@ -38,41 +30,64 @@ def sarsa(env, num_episodes, discount_rate=1.0, learning_rate=0.5, epsilon=0.2):
     - env: environment
     - num_episodes: # of episodes to run
     - discount_rate (gamma): how much to discount future rewards
-    - learning_rate (alpha): how much to update Q based on a given TD-error
+    - alpha: how much to update Q based on a given TD-error
     - epsilon: probability of "exploring" or choosing a random action
 
     Output:
     - Q: optimal state-value function
+    - stats: statistics of interest during training
     """
     Q = initialize_state_action_values(env.nS, env.nA)
+    stats = dict(episode_total_rewards=[], episode_total_steps=[])
 
     for t in range(num_episodes):
         state = env.reset()
-        action = get_greedy_action(Q, state)
+        action = get_epsilon_greedy_action(env, Q, state, epsilon)
         done = False
+        reward_sum = 0
+        step_counter = 0
 
         while not done:
             next_state, reward, done, _ = env.step(action)
+            next_action = get_epsilon_greedy_action(env, Q,
+                                                    next_state, epsilon)
 
-            if random.uniform(0.0, 1.0) < epsilon:
-                next_action = random.choice(tuple(range(env.nA)))
-            else:
-                next_action = get_greedy_action(Q, state)
-
-            Q[state][action] += learning_rate * (
-                reward + discount_rate * Q[next_state][next_action] -
-                Q[state][action])
+            TD_target = reward + discount_rate * Q[next_state][next_action]
+            TD_error = TD_target - Q[state][action]
+            Q[state][action] += alpha * TD_error
 
             state = next_state
             action = next_action
+
+            reward_sum += reward
+            step_counter += 1
+
+        stats['episode_total_rewards'].append(reward_sum)
+        stats['episode_total_steps'].append(step_counter)
      
-    return Q
+    return Q, stats
 
 
 def main():
     env = gym.make('CliffWalking-v0')
     env.seed(SEED)
-    Q = sarsa(env, 10)
+    Q, stats = sarsa(env, 1000)
+
+    fig, ax = plt.subplots(ncols=2)
+    ax[0].plot(stats['episode_total_rewards'])
+    ax[1].plot(stats['episode_total_steps'])
+
+    ax[0].set_title('SARSA Training Episode Rewards')
+    ax[0].set_xlabel('episode number')
+    ax[0].set_ylabel('episode reward sum')
+    ax[1].set_title('SARSA Training Episode Lengths')
+    ax[1].set_xlabel('episode number')
+    ax[1].set_ylabel('episode total steps')
+
+    plt.show()
+    plt.close()
+
+    watch_greedy_policy(env, Q)
 
 
 if __name__ == "__main__":
