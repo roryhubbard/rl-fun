9a92d63fe39ae5fecfbc1092eee239085cc75d9e
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-06-05 11:15:18 -0500

    using padding to transform 80x80 image to 84x84

--- DQN/q_network.py
+++ DQN/q_network.py
@@ -7,13 +7,12 @@ class QNetwork(nn.Module):
 
     def __init__(self, n_actions):
         super().__init__()
-        # (in_channels, out_channels, kernel_size)
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        # padding to transform 80 x 80 -> 84 x 84
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, padding=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
 
-        # compute 3136
-        self.fl1 = nn.Linear(6 * 6 * 64, 512)
+        self.fl1 = nn.Linear(7 * 7 * 64, 512)
         self.fl2 = nn.Linear(512, n_actions)
 
     def forward(self, x):
