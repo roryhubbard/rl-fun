4b817868f3237a275fd51c3b214e1d924649b627
Rory <calvin3h@gmail.com>
2021-11-01 14:32:58 +0100

    optimization loop code complete, backprop still needs to be implemented

--- policy_optimization/ppo.py
+++ policy_optimization/ppo.py
@@ -11,13 +11,14 @@ def main():
   T = 2048 # environement steps per update
   batch_size = 64
   epochs = 10
+  lr = 0.01
   discount = 0.99
   clipping_epsilon = 0.2
   lam = 0.95 # GAE parameter
   total_timesteps = 1000000
 
-  actor = Actor()
-  critic = Critic()
+  actor = Actor(lr=lr)
+  critic = Critic(lr=lr)
 
   n_updates = total_timesteps // T
   n_batches_per_update = T // batch_size + 1
@@ -28,7 +29,7 @@ def main():
     states = np.empty(T)
     actions = np.empty(T)
     rewards = np.empty(T)
-    returns = np.empty(T) # discounted
+    returns = np.empty(T) # discounted rewards
     dones = np.empty(T)
     state_values = np.empty(T+1)
     log_probs = np.empty(T)
@@ -59,35 +60,41 @@ def main():
     next_non_terminal = 1 - dones
     deltas = rewards + next_non_terminal * discount * state_values[1:] - state_values[:-1]
     gae = 0
-    for i in reversed(range(len(deltas))):
+    for i in reversed(range(T)):
       gae = delta[i] + next_non_terminal[i] * discount * lam * gae
       advantages[i] = gae
       discounted_reward = rewards[i] + next_non_terminal[i] * discount * discounted_reward
       returns[i] = discounted_reward
 
-    rng = np.random.default_rng()
     idx = np.arange(T)
+    actor_losses = []
+    critic_losses = []
     for k in range(epochs):
-      rng.shuffle(idx)
+      np.random.default_rng().shuffle(idx)
       for n in range(0, n_batches_per_update, batch_size):
         batch_idx = idx[n:n+batch_size]
         batch_states = states[batch_idx]
         batch_actions = actions[batch_idx]
-        batch_rtg = rewards_to_go[batch_idx]
+        batch_returns = returns[batch_idx]
         batch_state_values = state_values[batch_idx]
         batch_advantages = advantages[batch_idx]
         batch_log_probs = log_probs[batch_idx]
 
         _, current_log_probs = actor.forward(batch_states, batch_actions, grad=True)
-
-        ratios = np.exp(np.array(current_log_probs) - np.array(batch_log_probs))
+        ratios = np.exp(current_log_probs - batch_log_probs)
         clipped_ratios = np.minimum(1+clipping_epsilon,
                                     np.maximum(1-clipping_epsilon, ratios))
         actor_loss = -np.minimum(ratios * batch_advantages,
                                  clipped_ratios * batch_advantages).mean()
 
         current_state_values = critic.forward(batch_states, grad=True)
-        critic_loss = np.square(batch_rtg - current_state_values).mean()
+        critic_loss = np.square(batch_returns - current_state_values).mean()
+
+        actor.backward()
+        critic.backward()
+
+        actor_losses.append(actor_loss)
+        critic_losses.append(critic_loss)
 
   env.close()
 
