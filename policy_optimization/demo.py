5133b8af1d67daaaa2e60d49e257a83795f10023
Rory Hubbard <calvin3h@gmail.com>
2021-11-05 22:31:09 +0100

    view policy for double inverted pendulum

--- policy_optimization/demo.py
+++ policy_optimization/demo.py
@@ -3,14 +3,17 @@ import torch
 
 
 def main():
-  actor = torch.load('actor.pt').eval()
   env = gym.make('InvertedPendulum-v2')
+  actor = torch.load('actor_InvertedPendulum-v2.pt').eval()
+
+  #actor = torch.load('actor_InvertedDoublePendulum-v2.pt').eval()
+  #env = gym.make('InvertedDoublePendulum-v2')
 
   state = env.reset()
   done = False
   while not done:
     state_tensor = torch.as_tensor(state, dtype=torch.float32)
-    state, *_ = env.step(actor(state_tensor)[0])
+    state, _, done, _ = env.step(actor(state_tensor)[0])
     env.render()
 
 
